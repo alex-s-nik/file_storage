ed9e5bc8ff36e3a839e63b2e64c0bb49e5b52514
alex-s-nik <to_ctrl@mail.ru>
2023-06-06 11:24:20 +0300

    feat: add file model

---
+++ src/models/files.py
@@ -0,0 +1,20 @@
+import uuid
+
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, Text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+
+from db.db import BaseModel
+from .users import User
+
+
+class File(BaseModel):
+    __tablename__ = 'files'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(Text, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now, nullable=False)
+    path = Column(Text, nullable=False)
+    size = Column(Integer, nullable=False)
+    is_downloadable = Column(Boolean, server_default=True, nullable=False)
+    user_id = Column(UUID, ForeignKey(User.id, ondelete="CASCADE"))
